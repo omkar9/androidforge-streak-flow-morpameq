8217eb2290bc81cc8e4b74f59fc3443cf1acd59e
omkar9 <omkar946@gmail.com>
2026-05-05 02:48:22 +0530

    feat: add UpdateHabitUseCase.kt

---
+++ domain/src/main/java/com/androidforge/streakflow/domain/usecase/habit/UpdateHabitUseCase.kt
@@ -0,0 +1,19 @@
+package com.androidforge.streakflow.domain.usecase.habit
+
+import com.androidforge.streakflow.core.common.Result
+import com.androidforge.streakflow.domain.model.Habit
+import com.androidforge.streakflow.domain.repository.HabitRepository
+import javax.inject.Inject
+
+class UpdateHabitUseCase @Inject constructor(
+    private val habitRepository: HabitRepository
+) {
+    suspend operator fun invoke(habit: Habit): Result<Unit> {
+        return try {
+            habitRepository.updateHabit(habit)
+            Result.Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(e, "Failed to update habit.")
+        }
+    }
+}
